013b9325cef6ce9bfb16370b3a131f2381d98948
mkalmeida <michaellasfalmeida@gmail.com>
2023-05-31 14:39:47 -0300

    valores de notas restantes WIP

--- Caixa/AnaliseNotas.java
+++ Caixa/AnaliseNotas.java
@@ -7,8 +7,8 @@ public class AnaliseNotas {
 
 	Scanner input = new Scanner(System.in);
 		
-	int n50=0, n10=0, n5=0, n1=0, Rn50=0, Rn10=0, Rn5=0, Rn1=0, valorSaque, valorRestante=0, saqueTotal = 0;
-	int Ln50=0, Ln10=0, Ln5=0, Ln1=0, saques = 0, valor =0, i=0;
+	int n50=0, n10=0, n5=0, n1=0, Rn50=100, Rn10=100, Rn5=100, Rn1=100, valorSaque, valorRestante=0, saqueTotal = 0;
+	int Ln50=0, Ln10=0, Ln5=0, Ln1=0, saques = 0, valor =0, i=0, opcao = 0;
 	boolean validaNotas = false;
 		
 		
@@ -16,41 +16,70 @@ public class AnaliseNotas {
 	do {
 	System.out.println("Digite o valor do saque");
 	valorSaque = input.nextInt();
+	if (valorSaque > 1000) {
+		System.out.println("VALOR INVÁLIDO");
+		System.out.println("DIGITE: ");
+		System.out.println("1 - PARA DIGITAR VALOR DE SAQUE MENOR QUE B$ 1000" + "\n" + "2 - SAIR");
+		opcao = input.nextInt();
+		switch (opcao) {
+		case 1: 
+			System.out.println("Digite o valor do saque");
+			valorSaque = input.nextInt();
+			break;
+		case 2:
+			System.out.println("Você digitou sair");
+			System.out.println("Volte sempre");
+			System.exit(0);
+		}
+		
+	}
+	valor = valorSaque;
 	saques = saques + 1;
 		
 	if(valorSaque>=50) {
 	n50 = (int)valorSaque/50;
-	valorRestante = valorSaque - 50*n50;
-	Rn50 = 100 - n50 - Rn50;
-	System.out.println("valor restante: 50 " + valorRestante);
-	
+	valorSaque = valorSaque - 50*n50;
+	Rn50 = Rn50 - n50;
+	if (Rn50 < 1) {
+		System.out.println("Não há notas de B$ 50 suficientes");
+	}
+	continue;
 	}
 	
-	if(valorRestante>=10) {
-	n10 = (int)valorRestante/10;
-	valorRestante = valorRestante - 10*n10;
-	Rn10 = 100 - n10 - Rn10;
-	System.out.println("valor restante: 10 " + valorRestante);
+	if(valorSaque>=10) {
+	n10 = (int)valorSaque/10;
+	valorSaque = valorSaque - 10*n10;
+	Rn10 = Rn10 - n10;
+	if (Rn10 < 1) {
+		System.out.println("Não há notas de B$ 10 suficientes");
+	}
+	continue;
 	}
 		
-	if(valorRestante>=5) {
-	n5 = (int)valorRestante/5;
-	valorRestante = valorRestante - 5*n5;
-	Rn5 = 100 - n5 - Rn5;
-	System.out.println("valor restante: 5 " + valorRestante);
+	if(valorSaque>=5) {
+	n5 = (int)valorSaque/5;
+	valorSaque = valorSaque - 5*n5;
+	Rn5 = Rn5 - n5;
+	if (Rn5 < 1) {
+		System.out.println("Não há notas de B$ 5 suficientes");
+	}
+	continue;
 	}
 			
-	if(valorRestante>=1) {
-	n1 = (int)valorRestante/1;
-	valorRestante = valorRestante - 1*n5;
-	Rn1 = 100 - n1 - Rn1;
-	System.out.println("valor restante: 1 " + valorRestante);
+	if(valorSaque>=1) {
+	n1 = (int)valorSaque/1;
+	valorSaque = valorSaque - 1*n5;
+	Rn1 = Rn1 - n1;
+	if (Rn1 < 1) {
+		System.out.println("Não há notas de B$ 1 suficientes");
+	}
 	}	
 			
-	} while (valorSaque > 0 && saques <= 2);
+	} while (valor > 0 && saques <= 6);
 	
 	System.out.println("O valor solicitado é: " + valorSaque);
 	System.out.println("Restam " +Rn50+ " notas de B$ 50, " +Rn10+ " notas de B$ 10, " +Rn5+ " notas de R$ 5 e " +Rn1+ " notas de B$ 1.");
 	
 	}
+	
 }
